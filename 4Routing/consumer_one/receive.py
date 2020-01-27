7fb99caa3972710599aa37f7ca36d6e0aeb3957f
ldynia <ludd@cbs.dtu.dk>
2020-01-27 12:06:35 +0100

    adding Topics tutoiral

--- 4Routing/consumer_one/receive.py
+++ 4Routing/consumer_one/receive.py
@@ -14,23 +14,22 @@ channel.exchange_declare(exchange=EXCHANGE, exchange_type='direct')
 
 # once the consumer connection is closed, the queue is deleted -exclusive flag
 result = channel.queue_declare(queue='', exclusive=True)
-random_queue_name = result.method.queue
+queue_name = result.method.queue
 
 
 severities = sys.argv[1:]
 if not severities:
-    sys.stderr.write("Usage: %s [info] [warning] [error]\n" % sys.argv[0])
+    sys.stderr.write("Usage: %s [error]\n" % sys.argv[0])
     sys.exit(1)
 
-SEVERITIES = ['error']
-for severity in SEVERITIES:
-    channel.queue_bind(exchange=EXCHANGE, queue=random_queue_name, routing_key=severity)
+for severity in severities:
+    channel.queue_bind(exchange=EXCHANGE, queue=queue_name, routing_key=severity)
 
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r:%r" % (method.routing_key, body))
 
 
-channel.basic_consume(queue=random_queue_name, auto_ack=True, on_message_callback=callback)
+channel.basic_consume(queue=queue_name, auto_ack=True, on_message_callback=callback)
 
 channel.start_consuming()
--- 5Topics/consumer_three/receive.py
+++ 5Topics/consumer_three/receive.py
@@ -9,21 +9,20 @@ channel = connection.channel()
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 
-EXCHANGE = 'direct_logs'
-channel.exchange_declare(exchange=EXCHANGE, exchange_type='direct')
+EXCHANGE = 'topic_logs'
+channel.exchange_declare(exchange=EXCHANGE, exchange_type='topic')
 
 # once the consumer connection is closed, the queue is deleted -exclusive flag
 result = channel.queue_declare(queue='', exclusive=True)
 queue_name = result.method.queue
 
-
-severities = sys.argv[1:]
-if not severities:
-    sys.stderr.write("Usage: %s [info] [warning]\n" % sys.argv[0])
+binding_keys = sys.argv[1:]
+if not binding_keys:
+    sys.stderr.write("Usage: %s [binding_key]...\n" % sys.argv[0])
     sys.exit(1)
 
-for severity in severities:
-    channel.queue_bind(exchange=EXCHANGE, queue=queue_name, routing_key=severity)
+for bk in binding_keys:
+    channel.queue_bind(exchange=EXCHANGE, queue=queue_name, routing_key=bk)
 
 
 def callback(ch, method, properties, body):
